61c7f0a1568f7f930fe63d3c93c12f4b4802ccb7
Oge Nnadi <oge@nnadi.org>
2013-07-17 11:26:38 -0400

    did MOSI. On 4proc validation the himBH$ transfers are off by 4

--- protocols/MOSI_protocol.h
+++ protocols/MOSI_protocol.h
@@ -12,7 +12,11 @@ typedef enum {
     MOSI_CACHE_I = 1,
     MOSI_CACHE_S,
     MOSI_CACHE_O,
-    MOSI_CACHE_M
+    MOSI_CACHE_M,
+    MOSI_CACHE_IS,
+    MOSI_CACHE_IM,
+    MOSI_CACHE_SM,
+    MOSI_CACHE_OM
 } MOSI_cache_state_t;
 
 class MOSI_protocol : public Protocol {
@@ -30,11 +34,19 @@ public:
     inline void do_cache_S (Mreq * request);
     inline void do_cache_O (Mreq * request);
     inline void do_cache_M (Mreq *request);
+    inline void do_cache_IS (Mreq *request);
+    inline void do_cache_IM (Mreq *request);
+    inline void do_cache_SM (Mreq *request);
+    inline void do_cache_OM (Mreq *request);
 
     inline void do_snoop_I (Mreq *request);
     inline void do_snoop_S (Mreq *request);
     inline void do_snoop_O (Mreq *request);
     inline void do_snoop_M (Mreq *request);
+    inline void do_snoop_IS (Mreq *request);
+    inline void do_snoop_IM (Mreq *request);
+    inline void do_snoop_SM (Mreq *request);
+    inline void do_snoop_OM (Mreq *request);
 };
 
 #endif // _MOSI_CACHE_H
